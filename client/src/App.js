122c4a3e8352d6b509232656d4ac86170129e1b2
Heritier Akilimali <herri_93@hotmail.com>
2021-05-30 19:20:10 +0200

    removed bootstrap import

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 
 import bootstrap from "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap";
+
 import Navbar from "./components/Navbar";
 import Homescreen from "./screens/Homescreen";
 import Cartscreen from "./screens/Cartscreen";
